866abfff4778a1a8278a820f35d44815b7332a3f
vadhi raj <95899338+ihdavjar@users.noreply.github.com>
2023-06-17 12:16:26 +0530

    Delete Average directory

--- Average/Loss_grp.m
+++
@@ -1,7 +0,0 @@
-function [loss] = Loss_grp(model,x_test,y_test)
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-y_pred=predict(model,x_test);
-y_pred=abs(y_test-y_pred);
-loss=sum(y_pred)/(length(y_pred));
-end
